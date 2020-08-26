de59802c4aa99ce0772f5cb0dd740efce3593b74
pintu1481999 <pintu1481999@gmail.com>
2020-08-26 09:54:39 +0530

    secret key removed

--- moviedb/settings.py
+++ moviedb/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&o(f1xt1%&c29dbfcr4e)^f4g)vga_7u$10y9%d=ak+l$67bfb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
